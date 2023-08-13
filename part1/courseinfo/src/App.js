06f5cfe9f989f4d8cf765975267415377e230626
Dodi Yulian <cdoditri@gmail.com>
2023-08-13 19:00:46 +0700

    1.2: Done

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -1,3 +1,11 @@
+const Part = (props) => {
+  return (
+    <div>
+     <p>{props.part} {props.exercises}</p>
+    </div>
+  )
+}
+
 const Header = (props) => {
   return (
     <div>
@@ -6,9 +14,12 @@ const Header = (props) => {
   )
 }
 const Content = (props) => {
+  console.log(props.parts[0])
   return (
     <div>
-      <p>{props.part} {props.exercises}</p>
+      <Part part={props.parts[0]} exercises={props.exercises[0]} />
+      <Part part={props.parts[1]} exercises={props.exercises[1]}/>
+      <Part part={props.parts[2]} exercises={props.exercises[2]}/>
     </div>
   )
 }
@@ -32,9 +43,7 @@ const App = () => {
   return (
     <div>
       <Header course={course} />
-      <Content part={part1} exercises={exercises1} />
-      <Content part={part2} exercises={exercises2} />
-      <Content part={part3} exercises={exercises3} />
+      <Content parts={[part1, part2, part3]} exercises={[exercises1, exercises2, exercises3]} />
       <Total totalExercises={exercises1 + exercises2 + exercises3} />
     </div>
   )
